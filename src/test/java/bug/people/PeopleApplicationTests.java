559d9065ba2b4b7f5854f949aa466adefba35624
Iaven00 <1245325661@qq.com>
2022-09-25 16:58:29 +0800

    初始化文件

---
+++ src/test/java/bug/people/PeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package bug.people;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
