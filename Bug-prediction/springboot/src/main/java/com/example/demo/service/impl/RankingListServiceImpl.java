57e0adb7b6fb0758cd68667d79e0d1ba4bff2fa5
Wang Jiong <911124552@qq.com>
2022-09-28 20:21:09 +0800

    2022/09/28 后端接口 wj

---
+++ Bug-prediction/springboot/src/main/java/com/example/demo/service/impl/RankingListServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.RankingListService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RankingListServiceImpl implements RankingListService {
+}
---
+++ Bug-prediction/springboot/src/main/java/com/example/demo/service/impl/UserTrainInfoServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.UserTrainInfoService;
+
+public class UserTrainInfoServiceImpl implements UserTrainInfoService {
+}
