ee554443a4cde43ffb000657508410c5e10e1ee1
Iaven00 <1245325661@qq.com>
2022-10-09 17:14:25 +0800

    knn增加了成员k和weight

--- Bug-prediction/springboot/src/main/java/com/example/demo/service/KNN.java
+++ Bug-prediction/springboot/src/main/java/com/example/demo/service/KNN.java
@@ -6,8 +6,8 @@ public interface KNN {
     // 计算两个向量之间的距离
     double calDistance(ArrayList<Double> data1, ArrayList<Double> data2);
     // 返回类别,1代表没缺陷，0代表有缺陷，其他代表bug的种类
-    int knn(ArrayList<Double> newdata, int k,int weight);
-    double precise_weight(int w); // 带权重预测
     double precise(); // 无权重预测
     int knn(ArrayList<Double> newdata, int k);
+    void init(String file); //默认构造，k=5，weight = 1
+    void init(String file,int ku,int weightu);
 }
--- Bug-prediction/springboot/src/main/java/com/example/demo/service/impl/KNNSer.java
+++ Bug-prediction/springboot/src/main/java/com/example/demo/service/impl/KNNSer.java
@@ -14,8 +14,10 @@ public class KNNSer implements KNN {
     ArrayList<Pair<Double, Double>> dataset; // 新点到各点的距离,和各点的类别
     ArrayList<ArrayList<Double>> data_train; // 数据集;
     ArrayList<ArrayList<Double>> data_test;
-    int length;
-    int size;
+    int length;    // 维度
+    int size;      //数据集大小
+    int k;         //knn
+    int weight;    //负例的权重
     Comparator<Pair<Double, Double>> gc =new Comparator<Pair<Double, Double>>() {
         @Override
         public int compare(Pair<Double, Double> s1, Pair<Double, Double> s2) {
@@ -23,6 +25,7 @@ public class KNNSer implements KNN {
             return 1;
         }
     };
+    @Override
     public void init(String file){
         ArrayList<ArrayList<Double>> data = FileProcessImpl.read_csv(file,true);
         data_test = new ArrayList<>();
@@ -32,11 +35,32 @@ public class KNNSer implements KNN {
             if(i%5==0) data_test.add(data.get(i));
             if(i%5!=0) data_train.add(data.get(i));
         }
+        k=5;
+        weight = 1;
         length = data_train.get(0).size();
         size = data_train.size();
         System.out.println("初始化已完成 训练集："+size+"*"+length+"  测试集："+data_test.size()+"*"+length);
         System.out.println("KNN初始化完成");
     }
+
+    @Override
+    public void init(String file, int ku, int weightu) {
+        ArrayList<ArrayList<Double>> data = FileProcessImpl.read_csv(file,true);
+        data_test = new ArrayList<>();
+        data_train = new ArrayList<>();
+        dataset = new ArrayList<>();
+        for(int i=0;i<data.size();i++){
+            if(i%5==0) data_test.add(data.get(i));
+            if(i%5!=0) data_train.add(data.get(i));
+        }
+        k=ku;
+        weight = weightu;
+        length = data_train.get(0).size();
+        size = data_train.size();
+        System.out.println("初始化已完成 训练集："+size+"*"+length+"  测试集："+data_test.size()+"*"+length);
+        System.out.println("KNN初始化完成");
+    }
+
     @Override
     public double calDistance(ArrayList<Double> data1, ArrayList<Double> data2) {
         double result = 0;
@@ -46,29 +70,6 @@ public class KNNSer implements KNN {
         return Math.sqrt(result);
     }
 
-    @Override
-    public int knn(ArrayList<Double> newdata, int k,int weight) {
-        if(k>=data_test.size()) return 0;
-        int clean_weight=0;
-        int buggy_weight=0;
-        for(int i=0;i<size;i++){
-            double res = calDistance(data_train.get(i),newdata);
-            Pair<Double, Double> pair = Pair.of(res,data_train.get(i).get(length-1));
-//            System.out.println("第"+i+"个训练集的种类:"+data_train.get(i).get(length-1));
-            dataset.add(pair);
-        } // 求出新的点和所有点的距离
-        Collections.sort(dataset, gc); // 按距离从小到大排序
-        for(int i=0;i<k && k<data_test.size();i++){
-            double tem = dataset.get(i).getSecond();
-//            System.out.println("第"+i+"个dataset的种类:"+tem);
-            if(tem==0.0) clean_weight++;
-            if(tem==1.0) buggy_weight+=weight;
-        }
-//        System.out.print("clean:"+clean+"   buggy:"+buggy);
-        if(clean_weight>buggy_weight) return 1;
-        else if(clean_weight<buggy_weight) return 0;
-        return knn(newdata,k+1,weight);
-    }
     @Override
     public int knn(ArrayList<Double> newdata, int k) {
         if(k>=data_test.size()) return 0;
@@ -85,34 +86,12 @@ public class KNNSer implements KNN {
             double tem = dataset.get(i).getSecond();
 //            System.out.println("第"+i+"个dataset的种类:"+tem);
             if(tem==0.0) clean++;
-            if(tem==1.0) buggy++;
+            if(tem==1.0) buggy+= weight;
         }
 //        System.out.print("clean:"+clean+"   buggy:"+buggy);
         if(clean>buggy) return 1;
         else if(clean<buggy) return 0;
         return knn(newdata,k+1);
-    }
-    @Override
-    public double precise_weight(int w) {
-        int len = data_test.size(); //测试集数量
-        double clean_true = 0; //正例
-        double tp = 0;       // 预测正确的正例
-        double buggy_true = 0;
-        double fp = 0;
-        for(int i=0;i<len;i++){
-            int j =knn(data_test.get(i),13,w);
-            if(data_test.get(i).get(length-1) == 0.0){ // 真实数据为clean
-                clean_true++;
-                if(j==1) tp++; //系统划分为clean
-            }
-            if(data_test.get(i).get(length-1) == 1.0){ // 真实数据为buggy
-                buggy_true++;
-                if(j==0) fp++; //系统划分为buggy
-            }
-        }
-        System.out.println("finally:   TP:"+tp+" ; 正例:"+clean_true);
-        System.out.println("finally:   FP:"+fp+" ; 反例:"+buggy_true);
-        return tp/clean_true;
     }
     @Override
     public double precise() {
@@ -122,7 +101,7 @@ public class KNNSer implements KNN {
         double buggy_true = 0;
         double fp = 0;
         for(int i=0;i<len;i++){
-            int j =knn(data_test.get(i),13);
+            int j =knn(data_test.get(i),k);
             if(data_test.get(i).get(length-1) == 0.0){ // 真实数据为clean
                 clean_true++;
                 if(j==1) tp++; //系统划分为clean
@@ -140,7 +119,7 @@ public class KNNSer implements KNN {
     public static void main(String[] args){
         KNNSer knnSer = new KNNSer();
         knnSer.init("C:\\Users\\FUBOFENG\\Desktop\\实训1\\Bug-prediction\\springboot\\src\\main\\resources\\static\\JDT.csv");
-        System.out.println("准确率："+knnSer.precise_weight(5));
+        System.out.println("准确率："+knnSer.precise());
 //        knnSer.knn(knnSer.data_test.get(187),101);
     }
 }
